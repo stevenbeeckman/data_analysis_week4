9c0a6a1d8344fe0cb8c856bd83ed8e9a62d2b6cb
Steven Beeckman <steven.beeckman@gmail.com>
2014-01-28 18:24:10 +0100

    colorRampPalette()

--- 1_plotting_colors.R
+++ 1_plotting_colors.R
@@ -1,6 +1,13 @@
 stuffrogersaid = function(){
 	pal = colorRamp(c("red", "blue"))
+	print("pal only accepts values between 0 and 1")
 	print(pal(0))
 	print(pal(1))
 	print(pal(0.5))
+	print(pal(seq(0,1, len = 10)))
+
+	pal = colorRampPalette(c("red", "yellow"))
+	print("pal now accepts integer values, which returns a number of interpolated colours")
+	print(pal(2))
+	print(pal(10))
 }
